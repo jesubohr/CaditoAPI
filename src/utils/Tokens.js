4c1b60de672e571b59ea521563625c9efafd1fec
Jesús Borrero <39781228+jesubohr@users.noreply.github.com>
2022-04-24 01:25:46 -0500

    Token module for token-based sessions

---
+++ src/utils/Tokens.js
@@ -0,0 +1,11 @@
+const SessionToken = {
+    tokens: [],
+    add: function(token) {
+        this.tokens.push(token);
+    },
+    find: function(token) {
+        return this.tokens.find(t => t === token);
+    }
+}
+
+module.exports = SessionToken;
