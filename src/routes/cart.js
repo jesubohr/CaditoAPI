1f7a55abb1be0779f184f8caabf191ac79f80b78
Jesús Borrero <39781228+jesubohr@users.noreply.github.com>
2022-05-15 23:29:53 -0500

    Added trycatch on await clauses on routes

--- src/routes/cart.js
+++ src/routes/cart.js
@@ -5,18 +5,34 @@ const ShoppingCart = require('../utils/Cart');
 
 router.get('/', async (req, res) => {
     const { user_id } = req.query;
-    const user = await User.findById({ _id: user_id });
-    if (!user) return res.status(404).json({ error: 'User not found' });
-    res.json(ShoppingCart.get(user_id));
+    if (!user_id) return res.status(400).send({ error: 'Missing user_id' });
+
+    try {
+        const user = await User.findById({ _id: user_id });
+        if (!user) return res.status(404).json({ error: 'User not found' });
+        res.json(ShoppingCart.get(user_id));
+    } catch (error) {
+        return res.status(500).send({ error: 'Invalid user_id' });
+    }
 });
 
 router.post('/', async (req, res) => {
     const { user_id, product_id } = req.body;
-    const user = await User.findById(user_id);
-    if (!user) return res.status(404).json({ error: 'User not found' });
+    if (!user_id || !product_id) return res.status(400).send({ error: 'Missing fields' });
 
-    const product = await Product.findById({ _id: product_id });
-    if (!product) return res.status(404).json({ error: 'Product not found' });
+    try {
+        const user = await User.findById(user_id);
+        if (!user) return res.status(404).json({ error: 'User not found' });
+    } catch (error) {
+        return res.status(500).send({ error: 'Invalid user_id' });
+    }
+
+    try {
+        const product = await Product.findById({ _id: product_id });
+        if (!product) return res.status(404).json({ error: 'Product not found' });
+    } catch (error) {
+        return res.status(500).send({ error: 'Invalid product_id' });
+    }
 
     ShoppingCart.add(user_id, product_id);
     return res.json({ success: 'Product added to cart' });
@@ -24,17 +40,26 @@ router.post('/', async (req, res) => {
 
 router.post('/buy', async (req, res) => {
     const { user_id } = req.body;
-    const user = await User.findById(user_id);
-    if (!user) return res.status(404).json({ error: 'User not found' });
-    user.history.push(...ShoppingCart.buy(user_id));
-    await user.save();
+    if (!user_id) return res.status(400).send({ error: 'Missing user_id' });
+
+    try {
+        const user = await User.findById(user_id);
+        if (!user) return res.status(404).json({ error: 'User not found' });
+
+        user.history.push(...ShoppingCart.buy(user_id));
+        await user.save();
+    } catch (error) {
+        return res.status(500).send({ error: 'Invalid user_id' });
+    }
     return res.json({ success: 'Cart bought' });
 });
 
 router.delete('/', async (req, res) => {
     const { item_id } = req.query;
+    if (!item_id) return res.status(400).send({ error: 'Missing item_id' });
+
     ShoppingCart.remove(item_id);
-    res.json({ success: 'Product removed from cart' });
+    return res.json({ success: 'Product removed from cart' });
 });
 
 module.exports = router;
